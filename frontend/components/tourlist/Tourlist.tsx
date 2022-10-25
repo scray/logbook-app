ea4c84959354206c9364ecb87cffb9adc4c38351
philirmscher <82280738+philirmscher@users.noreply.github.com>
2022-10-25 20:26:10 +0200

    [#1] style: added ability to get more information by pressing the travelbutton

--- frontend/components/tourlist/Tourlist.tsx
+++ frontend/components/tourlist/Tourlist.tsx
@@ -1,30 +1,56 @@
-import React from "react";
-import { Button, StyleSheet, View } from "react-native";
+import React, { useState } from "react";
+import { Button, StyleSheet, View, Text } from "react-native";
 import Coordinates from "../../model/Coordinates";
 import Tour from "../../model/Tour";
 
 let tourlist: Tour[] = [
   {userId: "Guenther69", travelId: "travelid1",waypoints: [new Coordinates(100,100,1),new Coordinates(101,100,2),new Coordinates(102,100,3)]},
-  {userId: "Guenther69", travelId: "travelid1",waypoints: [new Coordinates(1,100,4),new Coordinates(2,100,5),new Coordinates(3,100,6)]},
-  {userId: "Felix", travelId: "travelid1",waypoints: [new Coordinates(99,100,5),new Coordinates(98,100,40),new Coordinates(90,100,200)]}
+  {userId: "Guenther69", travelId: "travelid2",waypoints: [new Coordinates(1,100,4),new Coordinates(2,100,5),new Coordinates(3,100,6)]},
+  {userId: "Felix", travelId: "travelid3",waypoints: [new Coordinates(99,100,5),new Coordinates(98,100,40),new Coordinates(90,100,200)]}
 ]
 
+
+
 export default function Tourlist(){
+  
+    const [currentTour, setCurrentTour] = useState<Tour> ();
+
+    function LoadTour(selectedTour: Tour, index: number ){//loads selected Tour after pressing the tourbutton
+      setCurrentTour(selectedTour);
+    }
+
     return(
         <View style={styles.tourlistContainer}>
-          {tourlist.map(createTourButtons, tourlist)}
+          { currentTour ? (
+          <View>
+            <Button 
+              title={ "back" }
+              onPress = { () => setCurrentTour(undefined)}
+            />
+
+            <Text> { "Travel ID: " + currentTour.travelId } </Text>
+            <Text> { "User ID: " + currentTour.userId } </Text>
+            <Text> { "Starting Location: " + currentTour.waypoints[0].getLatitude() +  " + " + currentTour.waypoints[0].getLongitude() } </Text>
+          </View>
+          ) : (
+            tourlist.map((tour, index) => {
+              return (
+                <Button
+                  key = { index }
+                  title = { tour.waypoints[0].timestamp + " - " + tour.waypoints[tour.waypoints.length - 1].timestamp}
+                  onPress = { () => { LoadTour(tour, index)}}
+                />
+              )
+            })
+          )}
         </View>
+
     )
+
+
 }
 
-function createTourButtons(tour: Tour){
-  return(
-    <Button
-      title = { tour.waypoints[0].timestamp + " - " + tour.waypoints[tour.waypoints.length - 1].timestamp}
-      onPress = { () => { console.log("Clicked " + tour.waypoints[0].timestamp + " - " + tour.waypoints[tour.waypoints.length - 1].timestamp)}}
-    />
-  )
-}
+
 
 const styles = StyleSheet.create({
   tourlistContainer: {
