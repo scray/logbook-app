b124918ae733af9eacedf09cef22fa0b4d5e100b
timbx1 <82574355+timbx1@users.noreply.github.com>
2022-11-14 20:43:41 +0100

    Update: Assets
    assets updated

--- node-js/source/blockchain/asset.ts
+++ node-js/source/blockchain/asset.ts
@@ -1,9 +1,20 @@
 export class TemporaryAsset{
     data : string;
 }
+export class Waypoint{
+    longitude : number;
+    latitude : number;
+    timestamp : number;
+
+    constructor(longitude : number, latitude : number, timestamp : number){
+        this.latitude = latitude;
+        this.longitude = longitude;
+        this.timestamp = timestamp;
+    }
+}
 
 export class Asset{
     userId : string;
     travelId : string;
-    positions : string;
+    positions : Waypoint[];
 }
