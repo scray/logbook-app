4bdc946d42d158598f7049258ecb21f3c83fefbb
Beinßen, Felix <felix.beinssen@student.hs-anhalt.de>
2022-12-13 13:26:28 +0100

    #1 docs: added http request methods

---
+++ frontend/api/httpRequests.ts
@@ -0,0 +1,39 @@
+const url = "http://localhost:8080/tour-app/tours";
+
+export async function post(path: string, body: any) {
+    let response = await fetch(url + path, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(body)
+    });
+    return await response.json();
+}
+
+export async function get(path: string) {
+    let response = await fetch(url + path);
+    return await response.json();
+}
+
+export async function put(path: string, body: any) {
+    let response = await fetch(url + path, {
+        method: "PUT",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(body)
+    });
+    return await response.json();
+}
+
+export async function patch(path: string, body: any) {
+    let response = await fetch(url + path, {
+        method: "PATCH",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(body)
+    });
+    return await response.json();
+}
