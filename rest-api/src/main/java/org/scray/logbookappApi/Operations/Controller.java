005554fd43bed22ba085e04c1227c77bc94acf8b
Chafficui <felixbeinssen@gmail.com>
2023-01-12 20:37:55 +0000

    #7 fix: added missing exports

--- rest-api/src/main/java/org/scray/logbookappApi/Operations/Controller.java
+++ rest-api/src/main/java/org/scray/logbookappApi/Operations/Controller.java
@@ -1,15 +1,7 @@
 package org.scray.logbookappApi.Operations;
 
 import org.scray.logbookappApi.Objects.Waypoint;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import org.scray.logbookappApi.Objects.Tour;
 import com.google.gson.Gson;
