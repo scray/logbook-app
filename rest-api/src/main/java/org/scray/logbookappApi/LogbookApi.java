f6143921dbd72d7efeb719ef3cee41494c578f33
Roesch, Tristan <tristan.roesch@student.hs-anhalt.de>
2023-01-13 15:43:21 +0100

    [#7] build: check  connenction build up

--- rest-api/src/main/java/org/scray/logbookappApi/LogbookApi.java
+++ rest-api/src/main/java/org/scray/logbookappApi/LogbookApi.java
@@ -1,12 +1,20 @@
 package org.scray.logbookappApi;
 
+import java.util.logging.*;
+
+import org.scray.logbookappApi.Operations.Controller;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class LogbookApi {
+	static Logger logger = Logger.getLogger(Logger.GLOBAL_LOGGER_NAME);
 
-	public static void main(String[] args) {
-		SpringApplication.run(LogbookApi.class, args);
+	public static void main(String[] args) throws Exception {
+		if (Controller.blockchainOperations.connect() != null) {
+			SpringApplication.run(LogbookApi.class, args);
+		} else {
+			logger.info("Error: Connection failed to build up.");
+		}
 	}
 }
