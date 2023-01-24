6ec1fb45676fdcf8d3f77db22ba6ee732bbe091d
philirmscher <82280738+philirmscher@users.noreply.github.com>
2023-01-24 01:11:03 +0100

    #7 style: changed the background color of the app to a gradient

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -1,15 +1,23 @@
 import {StatusBar} from 'expo-status-bar';
 import {StyleSheet, View} from 'react-native';
 import Overview from './pages/Overview';
+import { useEffect, useState } from 'react';
+import { LinearGradient } from 'expo-linear-gradient';
 
 export default function App() {
+
     return (
         <View style={styles.container}>
-            <Overview/>
-            {
-                // <BlockchainRestTestComponent/>
-            }
-            <StatusBar style="auto"/>
+            <LinearGradient
+            colors={['#4c669f', '#3b5998', '#192f6a']}
+            style={styles.gradient}
+            start={[0, 0]}
+            end={[1, 1]}
+            >   
+                <Overview/>
+                <StatusBar style="auto"/>
+            </LinearGradient>
+
         </View>
     );
 }
@@ -17,8 +25,11 @@ export default function App() {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: '#fff',
         alignItems: 'center',
         justifyContent: 'center',
     },
+    gradient: {
+        height: "100%",
+        width: "100%"
+    }
 });
