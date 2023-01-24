b9f91a98a116075373ce3e8a3a1e649e4354f204
philirmscher <82280738+philirmscher@users.noreply.github.com>
2023-01-24 01:47:08 +0100

    #7 style: made changes to the navbar: buttons change color when selected different icon for the startbutton page

--- frontend/components/navigationBar/navigationBar.tsx
+++ frontend/components/navigationBar/navigationBar.tsx
@@ -4,38 +4,52 @@ import { MaterialIcons } from '@expo/vector-icons';
 import {useState} from "react";
 
 export default function NavigationBar({currentPage, setCurrentPage}: {currentPage : String, setCurrentPage: React.Dispatch<React.SetStateAction<string>>}){
+    
+    const styles = StyleSheet.create({
+        NavContainer: {
+            position: "absolute",
+            bottom: 0,
+            alignItems: "center",
+            zIndex: 99,
+        },
+        NavBar:{
+            flexDirection: "row",
+            width: "100%",
+            justifyContent: "space-evenly",
+            padding: 5,
+            borderTopWidth: 1,
+            borderColor: "#C1C1C1",
+        },
+        btn1:{
+            color: (currentPage === "starttour") ? ("white") : ("#C1C1C1"),
+        },
+        btn2:{
+            color: (currentPage === "tourmanagment") ? ("white") : ("#C1C1C1"),
+        },
+        btn3:{
+            color: (currentPage === "wallet") ? ("white") : ("#C1C1C1"),
+        },
+        btn4:{
+            color: (currentPage === "settings") ? ("white") : ("#C1C1C1"),
+        }
+    })
 
     return(
         <View style={styles.NavContainer}>
             <View style={styles.NavBar}>
                 <Pressable onPress={() => {setCurrentPage("starttour") }}>
-                    <MaterialIcons name="account-circle" size={32} color="white" />
+                    <Ionicons style={styles.btn1} name="play" size={32} color="white" />
                 </Pressable>
                 <Pressable onPress={() => {setCurrentPage("tourmanagment") }}>
-                    <MaterialIcons name="tour" size={32} color="white" />
+                    <MaterialIcons style={styles.btn2} name="tour" size={32} color="white" />
                 </Pressable>
-                <Pressable onPress={() => {setCurrentPage("wallet") }}>
-                    <Ionicons name="wallet" size={32} color="white" />
+                <Pressable onPress={() => {setCurrentPage("account-circle") }}>
+                    <Ionicons style={styles.btn3} name="wallet" size={32} color="white" />
                 </Pressable>
                 <Pressable onPress={() => {setCurrentPage("settings") }}>
-                    <Ionicons name="settings-outline" size={32} color="white" />
+                    <Ionicons style={styles.btn4} name="settings-outline" size={32} color="white" />
                 </Pressable>                
             </View>
         </View>
     );
 }
-
-const styles = StyleSheet.create({
-    NavContainer: {
-        position: "absolute",
-        bottom: 20,
-        alignItems: "center",
-    },
-    NavBar:{
-        flexDirection: "row",
-        backgroundColor: "green",
-        width: "95%",
-        justifyContent: "space-evenly",
-        borderRadius: 30,
-    }
-})
