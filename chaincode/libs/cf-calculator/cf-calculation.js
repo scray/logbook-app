fbacdfe46d3ac88eab4ffd0e13a49208ed4c63a5
sthahuze <halyna.huzenko@student.hs-anhalt.de>
2023-06-23 14:55:55 +0200

    Adding missing files distance calculation function

--- chaincode/libs/cf-calculator/cf-calculation.js
+++ chaincode/libs/cf-calculator/cf-calculation.js
@@ -4,3 +4,31 @@ function calculateCf(distance, emissionFactor) {
 }
 
 module.exports = calculateCf;
+
+function haversineDistance(lon1, lat1, lon2, lat2) {
+// Convert latitude and longitude from degrees to radians
+  const lat1Rad = (Math.PI / 180) * lat1;
+  const lon1Rad = (Math.PI / 180) * lon1;
+  const lat2Rad = (Math.PI / 180) * lat2;
+  const lon2Rad = (Math.PI / 180) * lon2;
+
+ // Radius of the Earth in kilometers
+  const radius = 6371;
+
+ // Difference between latitude and longitude
+  const deltaLat = lat2Rad - lat1Rad;
+  const deltaLon = lon2Rad - lon1Rad;
+
+  // Haversine formula
+  const a =
+    Math.sin(deltaLat / 2) ** 2 +
+    Math.cos(lat1Rad) * Math.cos(lat2Rad) * Math.sin(deltaLon / 2) ** 2;
+  const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+
+  // Distance between points
+  const distance = radius * c;
+
+  return distance;
+}
+
+module.exports = haversineDistance;
--- chaincode/libs/cf-calculator/cf-calculation.test.js
+++ chaincode/libs/cf-calculator/cf-calculation.test.js
@@ -1,5 +1,11 @@
 const calculateCf = require('./cf-calculation');
+const haversineDistance = require('./cf-calculation');
 
-test('calculate the cf of an truck', () => {
-  expect(calculateCf(4, 0.2)).toBe(0.8);
+
+//test('calculate the cf of an truck', () => {
+ // expect(calculateCf(4, 0.2)).toBe(0.8);
+//});
+
+test('calculate distance', () => {
+  expect(haversineDistance(50.4501, 30.5234, 52.2297, 21.0122)).toBe(1072.4479169072167);
 });
