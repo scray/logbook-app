a07df280a66c0e25eb09e47e07770661dd54dd5d
Stefan Obermeier <st.obermeier@seeburger.de>
2023-11-11 23:59:12 +0100

    Beautify test message

--- chaincode/release/blockchain/contract.js
+++ chaincode/release/blockchain/contract.js
@@ -125,7 +125,7 @@ class Contracts extends fabric_contract_api_1.Contract {
     getTours(context, userId) {
         return __awaiter(this, void 0, void 0, function* () {
             /* Request all tours from the given user with userId */
-            __1.Logger.write(logger_1.Prefix.NORMAL, " Test message of user test " + userId);
+            __1.Logger.write(logger_1.Prefix.NORMAL, " Test message of user: " + userId);
             __1.Logger.write(logger_1.Prefix.NORMAL, "Request entry with the id " + userId + " from the blockchain.");
             let bytes = yield context.stub.getState(userId);
             if (bytes.length <= 0) {
