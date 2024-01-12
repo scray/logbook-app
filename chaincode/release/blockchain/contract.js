0b19bb5c7ae105e2bcedfa041ac1e44026225b10
sthahuze <halyna.huzenko@student.hs-anhalt.de>
2024-01-12 11:42:11 +0100

    changes in contracts

--- chaincode/release/blockchain/contract.js
+++ chaincode/release/blockchain/contract.js
@@ -14,6 +14,8 @@ const fabric_contract_api_1 = require("fabric-contract-api");
 const __1 = require("..");
 const logger_1 = require("../logger");
 const asset_1 = require("./asset");
+const vehicle_1 = require("./vehicle")
+
 const haversineDistance = require("../../libs/cf-calculator/cf-calculation.js");
 class Contracts extends fabric_contract_api_1.Contract {
     constructor() {
@@ -139,5 +141,38 @@ class Contracts extends fabric_contract_api_1.Contract {
             return JSON.stringify(data.tours);
         });
     }
+
+    createTransport(context, transport) {
+            return __awaiter(this, void 0, void 0, function* () {
+                let data;
+                __1.Logger.write(logger_1.Prefix.NORMAL, "Trying to create a transport with ID " + transport.id + ".");
+                let bytes = yield context.stub.getState(transport.id);
+                if (bytes.length < 1) {
+                    data = new asset_1.Vehicle(transport.id, transport.name, transport.emissionPerKm);
+                } else {
+                    __1.Logger.write(logger_1.Prefix.WARNING, "Transport with ID " + transport.id + " already exists. Updating the data.");
+                    data = JSON.parse(bytes.toString());
+                    data.name = transport.name;
+                    data.emissionPerKm = transport.emissionPerKm;
+                }
+                context.stub.putState(transport.id, Buffer.from(JSON.stringify(data)));
+                __1.Logger.write(logger_1.Prefix.NORMAL, "Transport with ID " + transport.id + " has been created/updated.");
+                return JSON.stringify(data);
+            });
+        }
+
+        getTransport(context, transportId) {
+            return __awaiter(this, void 0, void 0, function* () {
+                __1.Logger.write(logger_1.Prefix.NORMAL, " Requesting transport with ID " + transportId + " from the blockchain.");
+                let bytes = yield context.stub.getState(transportId);
+                if (bytes.length <= 0) {
+                    __1.Logger.write(logger_1.Prefix.ERROR, "The required transport with ID " + transportId + " is not available.");
+                    return false;
+                }
+                let data = JSON.parse(bytes.toString());
+                __1.Logger.write(logger_1.Prefix.SUCCESS, "Transport with ID " + transportId + " has been found and sent to the requester.");
+                return JSON.stringify(data);
+            });
+        }
 }
 exports.Contracts = Contracts;
